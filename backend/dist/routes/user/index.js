a2ea55c16fc1c9602d2d8c915a1de4576ea03cc9
Leafaar <45247054+leafaar@users.noreply.github.com>
2022-09-25 11:36:39 -0300

    Delete backend/dist directory

--- backend/dist/routes/user/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const user_1 = require("../../controllers/user");
-const middlewares_1 = require("../../utils/middlewares");
-const router = (0, express_1.Router)();
-router.get('/', middlewares_1.isAuthenticated, user_1.getUserController);
-exports.default = router;
--- backend/dist/utils/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.JWT_SECRET = exports.DISCORD_API_URL = void 0;
-exports.DISCORD_API_URL = 'https://discord.com/api/v9';
-exports.JWT_SECRET = process.env.NEXT_PUBLIC_JWT_SECRET;
